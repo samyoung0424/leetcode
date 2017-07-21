bceade3c6dd26dfbb2cb8f3b9a6c108349f0f455
sylvanyang <sylvanyang0424@gmail.com>
2017-07-21 09:16:26 -0700

    passed

--- combination-sum-ii.py
+++ combination-sum-ii.py
@@ -1,1 +1,22 @@
+class Solution(object):
+    def dfs(self, nums, target, cur_sol, res):
+        if target == 0 :
+            res.append(cur_sol)
+            return
+        if target < 0:
+            return
+        for i in range(len(nums)):
+            if i==0 or nums[i] != nums[i-1]:
+                self.dfs(nums[i+1:], target-nums[i], cur_sol+[nums[i]], res)
+
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        res = []
+        self.dfs(candidates, target, [], res)
+        return res
 
