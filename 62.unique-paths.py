f32705a78ae34a9a90342eeadc322a96cc241207
sylvanyang <sylvanyang0424@gmail.com>
2017-07-24 23:32:49 -0700

    a dp method to solve this problem (good way)

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -5,5 +5,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        import math
-        return (math.factorial(m+n-2)/math.factorial(m-1)) / math.factorial(n-1)
+        # import math
+        # return (math.factorial(m+n-2)/math.factorial(m-1)) / math.factorial(n-1)
+        dp = [[0 for i in range(n)] for i in range(m)]
+        for i in range(0, m):
+            dp[i][0] = 1
+        for j in range(1, n):
+            dp[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
