be7954ebe61058fb3aca2e290e438ff517727339
sylvanyang <sylvanyang0424@gmail.com>
2017-08-03 17:06:34 -0700

    a mathematic method of generating grey code, very classsic

---
+++ 89.gray-code.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        size = 1 << n
+        for i in range(size):
+            res.append((i >> 1) ^ i)
+        return res
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.grayCode(2)
